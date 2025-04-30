ace52e43701530598356eb321a34d966d1b58223
Azatikone <azatikone@gmail.com>
2025-04-30 23:04:41 +0400

    Запрос файлов, загрузка файлов без папок.

--- 2.py
+++ 2.py
@@ -1,1 +1,17 @@
-155
+import requests
+import os
+
+download_folder = 'git_hellokuber_tenzor'
+owner = "paulbouwer"
+repo = "hello-kubernetes"
+path = "src/app"
+url = f"https://api.github.com/repos/{owner}/{repo}/contents/{path}"
+files = requests.get(url).json()
+
+for file in files:
+    if file['type'] == 'file':
+        download_url = file['download_url']
+        content = requests.get(download_url).content
+        filename = file['name']
+
+
