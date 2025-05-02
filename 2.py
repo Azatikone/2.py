73dc3336dc46d2588c8ae3131c7bd719984bc8e4
Azatikone <azatikone@gmail.com>
2025-05-02 17:13:49 +0400

    создание файла version, проработка 2 задания.

--- 2.py
+++ 2.py
@@ -1,5 +1,6 @@
 import requests
 import os
+import json
 
 download_fold='git_hellokuber_tenzor'
 os.makedirs(download_fold, exist_ok=True)
@@ -7,10 +8,25 @@ os.makedirs(download_fold, exist_ok=True)
 url = "https://api.github.com/repos/paulbouwer/hello-kubernetes/contents/src/app"
 files = requests.get(url).json()
 
-for file in files:
+'''for file in files:
     if file['type'] == 'file':
         download_url = file['download_url']
         content = requests.get(download_url).content
         filename = file['name']
         n=open(f"{download_fold}/{filename}",'wb')
-        n.write(content)
+        n.write(content)'''
+
+with open("git_hellokuber_tenzor/package.json") as f:
+    ver = json.load(f)['version']
+with open('version.json', 'r') as f:
+   data = json.load(f)
+   data['version']= ver
+with open('version.json', 'w') as f:
+   json.dump(data, f)
+
+names = [i for i in os.listdir('git_hellokuber_tenzor') if i.endswith('.js') or i.endswith('.py') or i.endswith('.sh')]
+with open('version.json', 'r+') as f:
+   data = json.load(f)
+   data['files']= names
+   f.seek(0)
+   json.dump(data, f)
